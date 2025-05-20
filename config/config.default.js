54bdc1fdc8262a8f703cc3f1b618b66dd610f649
左怀含 <5125534+zuohuaipan@user.noreply.gitee.com>
2025-05-20 18:48:05 +0800

    change:更改转换图片前缀，适配nginx

--- config/config.default.js
+++ config/config.default.js
@@ -36,7 +36,7 @@ module.exports = appInfo => {
   config.static = {
     dir: [
       {
-        prefix: '/public', // Web 访问前缀
+        prefix: '/api/public', // Web 访问前缀
         dir: path.join(appInfo.baseDir, 'app/public') // 本地存储目录
       }
     ]
